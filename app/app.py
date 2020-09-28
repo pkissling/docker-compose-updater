69c9c2225cf0e3a8348efcaca76bc516c1422b09
Patrick Kissling <patrick.kissling@de.bosch.com>
2020-09-28 17:22:14 +0200

    Env vars

--- app/app.py
+++ app/app.py
@@ -2,8 +2,14 @@ from flask import Flask
 import os
 app = Flask(__name__)
 
+API_KEY = os.environ['API_KEY']
+
 @app.route('/')
 def hello_world():
+    try:
+        os.environ["API_KEY"]
+    except KeyError:
+        return "nope", 401
     os.system('docker-compose -f ../resources/docker-compose.yaml up')
     return 'Hello, World!'
 
