e872028c0e3acc0d54994e2f78d4b168502417b7
Patrick Kissling <patrick.kissling@gmx.de>
2020-09-28 21:52:17 +0200

    Initial implementation

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,6 @@ COPY ./app /app
 WORKDIR /app
 RUN pip install -r requirements.txt
 
+EXPOSE 8080/tcp
+
 CMD ["flask", "run", "-h", "0.0.0.0", "-p", "5000"]
--- app/app.py
+++ app/app.py
@@ -1,15 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+import docker
 import os
+
 app = Flask(__name__)
 
 API_KEY = os.environ['API_KEY']
+docker_client = docker.from_env()
 
-@app.route('/')
-def hello_world():
+@app.route('/docker/containers/<container_name>/update')
+def update_container(container_name):
+
+    api_key = request.headers.get('X-API-Key')
+    if api_key != API_KEY:
+        return {}, 401
+
     try:
-        os.environ["API_KEY"]
-    except KeyError:
-        return "nope", 401
-    os.system('docker-compose -f ../resources/docker-compose.yaml up')
-    return 'Hello, World!'
+        docker_client.containers.get(container_name)
+    except docker.errors.NotFound:
+        return API_KEY, 404
+
+    os.system('docker-compose --file /resources/docker-compose.yaml pull {}'.format(container_name))
+    os.system('docker-compose --detach --file /resources/docker-compose.yaml up {}'.format(container_name))
+    os.system('docker image prune --force')
+    return {}, 200
 
