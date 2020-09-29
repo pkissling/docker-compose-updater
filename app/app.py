251d751bca0b72b1c1ce2f50befa6e8237f315ca
Patrick Kissling <patrick.kissling@gmx.de>
2020-09-29 11:32:23 +0200

    Docker compose down

--- app/app.py
+++ app/app.py
@@ -4,23 +4,25 @@ import os
 
 app = Flask(__name__)
 
-API_KEY = os.environ['API_KEY']
+EXPECTED_API_KEY = os.environ['API_KEY']
 docker_client = docker.from_env()
 
 @app.route('/docker/containers/<container_name>/update', methods=['PUT'])
 def update_container(container_name):
 
-    api_key = request.headers.get('X-API-Key')
-    if api_key != API_KEY:
-        return {}, 401
+    # actual_api_key = request.headers.get('X-API-Key')
+    # if actual_api_key != EXPECTED_API_KEY:
+    #     return {}, 401
 
-    try:
-        docker_client.containers.get(container_name)
-    except docker.errors.NotFound:
-        return API_KEY, 404
+    # try:
+    #     docker_client.containers.get(container_name)
+    # except docker.errors.NotFound:
+    #     return EXPECTED_API_KEY, 404
 
-    os.system('docker-compose --file /resources/docker-compose.yaml pull {}'.format(container_name))
-    os.system('docker-compose --file /resources/docker-compose.yaml up --detach {}'.format(container_name))
-    os.system('docker image prune --force')
+
+    os.system('docker-compose --file /resources/docker-compose.yaml down')
+    # os.system('docker-compose --file /resources/docker-compose.yaml pull {}'.format(container_name))
+    # os.system('docker-compose --file /resources/docker-compose.yaml up --detach {}'.format(container_name))
+    # os.system('docker image prune --force')
     return {}, 200
 
